99eac4568d890b531f35ac716ec74e540420d639
Jabir FTH <elftouhjabir@gmail.com>
2019-04-22 20:05:48 +0200

    Migration to Angular 7 (#1)
    - add mkdirp dependency to start project cross platform
    - update package.json and package-lock.json to build project with node v10.15.3 npm v6.4.1
    - Update rxjs version to 6.x
    - normalize principalType in order.json Role to uppercase

--- server/utils/ldap-role-resolver.ts
+++ server/utils/ldap-role-resolver.ts
@@ -1,7 +1,6 @@
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/bindNodeCallback';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
+import { bindNodeCallback, Observable, of as observableOf } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { providers } from '../providers';
 
 const ldapProviderOptions = providers ? providers.ldap : null;
@@ -21,19 +20,21 @@ export class LdapRoleResolver {
   private resolveLdapUserRoles(userId: number): Observable<string[]> {
     if (ldapProviderOptions) {
       const findOneUserIdentity: (filter) => Observable<string[]> =
-      Observable.bindNodeCallback(this.app.models.UserIdentity.findOne)
+      bindNodeCallback(this.app.models.UserIdentity.findOne)
         .bind(this.app.models.UserIdentity);
-      return findOneUserIdentity({ where: { userId } }).map((userIdentity: any) => {
-        if (userIdentity && userIdentity.provider === 'ldap') {
-          const existingRoles = Object.getOwnPropertyNames(ldapProviderOptions.rolesMapping);
-          return existingRoles.filter((role) => {
-            const ldapGroups = ldapProviderOptions.rolesMapping[role];
-            return this.isMemberOfAtLeastOneLdapGroup(userIdentity, ldapGroups);
-          });
-        }
-      });
+      return findOneUserIdentity({ where: { userId } }).pipe(
+        map((userIdentity: any) => {
+          if (userIdentity && userIdentity.provider === 'ldap') {
+            const existingRoles = Object.getOwnPropertyNames(ldapProviderOptions.rolesMapping);
+            return existingRoles.filter((role) => {
+              const ldapGroups = ldapProviderOptions.rolesMapping[role];
+              return this.isMemberOfAtLeastOneLdapGroup(userIdentity, ldapGroups);
+            });
+          }
+        }),
+      );
     }
-    return Observable.of([]);
+    return observableOf([]);
   }
 
   private isMemberOfAtLeastOneLdapGroup(userIdentity: any, ldapGroups: string[]) {
